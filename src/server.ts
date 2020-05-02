24c7ea5e5c58dcef0abab70fc33f56ce336ebe2f
Wanderson <wanderson.alves.rodrigues@gmail.com>
2020-05-02 14:08:39 -0300

    adição do debug

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,9 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (request, response) => response.json({ message: 'OPA' }));
+app.get('/', (request, response) => {
+  response.json({ message: 'OPA' });
+});
 
 app.listen(3333, () => {
   console.log('Server started on port 3333');
