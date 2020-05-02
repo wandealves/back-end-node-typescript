66a4ee85f7a68e6c2b347fe67f8084ca4db2a942
Wanderson <wanderson.alves.rodrigues@gmail.com>
2020-05-02 14:03:15 -0300

    adição da eslint e prittier

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,9 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({ message: "OPA" });
-});
+app.get('/', (request, response) => response.json({ message: 'OPA' }));
 
 app.listen(3333, () => {
-  console.log("Server started on port 3333");
+  console.log('Server started on port 3333');
 });
