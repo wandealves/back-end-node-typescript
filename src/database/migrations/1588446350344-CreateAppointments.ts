aef19a8b0b67cf725b9b0dbb1ebca5010520d10d
Wanderson <wanderson.alves.rodrigues@gmail.com>
2020-05-02 17:33:22 -0300

    criação do repositório e serviço de Appointment

--- src/database/migrations/1588446350344-CreateAppointments.ts
+++ src/database/migrations/1588446350344-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1588446350344
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
---
+++ src/services/CreateAppointmentService.ts
@@ -0,0 +1,37 @@
+import { startOfHour } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import Appointment from '../models/Appointment';
+
+interface Request {
+  provider: string;
+  date: Date;
+}
+
+class CreateAppointmentService {
+  public async execute({ date, provider }: Request): Promise<Appointment> {
+    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+
+    const appointmentDate = startOfHour(date);
+
+    const findAppointmentInSameDate = await appointmentsRepository.findByDate(
+      appointmentDate,
+    );
+
+    if (findAppointmentInSameDate) {
+      throw Error('this appointment is already booked');
+    }
+
+    const appointment = appointmentsRepository.create({
+      provider,
+      date: appointmentDate,
+    });
+
+    await appointmentsRepository.save(appointment);
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
