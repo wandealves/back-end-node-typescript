903f5531518f2f177cfe3214211c9349757d683a
Wanderson <wanderson.alves.rodrigues@gmail.com>
2020-06-07 14:32:40 -0300

    exibindo a imagem

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import 'reflect-metadata';
 import express from 'express';
 // import cors from 'cors';
 
+import uploadConfig from './config/upload';
+
 import routes from './routes';
 
 import './database';
@@ -10,6 +12,7 @@ const app = express();
 
 // app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.listen(3333, () => {
