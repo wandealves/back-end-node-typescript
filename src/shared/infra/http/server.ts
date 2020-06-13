31aa1828af140fb80c935c692756989019e080f4
Wanderson <wanderson.alves.rodrigues@gmail.com>
2020-06-13 13:01:26 -0300

    ajuste nas importações

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,14 +1,14 @@
 import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
-import 'express-async-errors';
 import cors from 'cors';
+import 'express-async-errors';
 
-import AppError from './errors/AppError';
-import uploadConfig from './config/upload';
+import uploadConfig from '@config/upload';
+import AppError from '@shared/errors/AppError';
 
 import routes from './routes';
 
-import './database';
+import '@shared/infra/typeorm';
 
 const app = express();
 
