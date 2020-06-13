8458100612c21dd4a623559521fae54788eb6daa
Wanderson <wanderson.alves.rodrigues@gmail.com>
2020-06-13 14:21:28 -0300

    alteração da resposabilidade dos serviços com os repositórios

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -3,6 +3,7 @@ import multer from 'multer';
 
 import uploadConfig from '@config/upload';
 
+import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 import CreateUserService from '@modules/users/services/CreateUserService';
 import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
 
@@ -14,7 +15,8 @@ const upload = multer(uploadConfig);
 usersRouter.post('/', async (request, response) => {
   const { name, email, password } = request.body;
 
-  const createUser = new CreateUserService();
+  const userRepository = new UsersRepository();
+  const createUser = new CreateUserService(userRepository);
   const user = await createUser.execute({
     name,
     email,
@@ -31,7 +33,8 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    const updateUserAvatar = new UpdateUserAvatarService();
+    const userRepository = new UsersRepository();
+    const updateUserAvatar = new UpdateUserAvatarService(userRepository);
     const user = await updateUserAvatar.execute({
       user_id: request.user.id,
       avatarFilename: request.file.filename,
