fd8f3c4cc0cb021f5a65e8ee51985abec777b3ef
Wanderson <wanderson.alves.rodrigues@gmail.com>
2020-06-14 11:24:36 -0300

    criação do teste do create appointment

--- src/tests/services/CreateAppointmentService.spec.ts
+++ src/tests/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,24 @@
-test('sum two numbers', () => {
-  expect(1 + 2).toBe(3);
+import 'reflect-metadata';
+import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+
+describe('CreateAppointment', () => {
+  it('should be able to crete a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointmentService = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointmentService.execute({
+      date: new Date(),
+      provider_id: '12345678',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('12345678');
+  });
+
+  // it('should not be able to create two appointment on the same time', () => {
+  //   expect('');
+  // });
 });
