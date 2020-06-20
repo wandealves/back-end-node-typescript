0432bcacb5db992f8e47cdb08448e6cce2964634
Wanderson <wanderson.alves.rodrigues@gmail.com>
2020-06-20 15:22:02 -0300

    retirada da responsabilidade de manipulação de arquivos

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,6 +1,8 @@
 import { container } from 'tsyringe';
 
+// import containers
 import '@modules/users/providers';
+import './providers';
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentRepository';
 import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
