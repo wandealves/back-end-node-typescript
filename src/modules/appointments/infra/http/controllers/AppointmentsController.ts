5c8e21c9d79e8dfc9b263f93ab6434dde58c3927
Wanderson <wanderson.alves.rodrigues@gmail.com>
2020-07-19 15:20:47 -0300

    adição da validações das rotas

--- src/modules/appointments/infra/http/controllers/AppointmentsController.ts
+++ src/modules/appointments/infra/http/controllers/AppointmentsController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { parseISO } from 'date-fns';
+import { parseISO, format } from 'date-fns';
 import { container } from 'tsyringe';
 
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
@@ -8,7 +8,8 @@ export default class AppointmentsController {
   public async create(request: Request, response: Response): Promise<Response> {
     const { provider_id, date } = request.body;
     const user_id = request.user.id;
-    const parseDate = parseISO(date);
+    const dateFormatted = format(date, 'yyyy-MM-dd HH:mm:ss');
+    const parseDate = parseISO(dateFormatted);
 
     const createAppointment = container.resolve(CreateAppointmentService);
 
